18395ddd6a403d91dbfe70da7446e472b8a94562
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-04-19 15:48:07 +0300

    Refactoring - rename short names to full names - change methods to get methods

--- Module2/Main.java
+++ Module2/Main.java
@@ -25,16 +25,15 @@ public class Main {
                 .sorted(Comparator
                         .comparingInt(Book::getPagesCount)) // - Отсортировать книги по количеству страниц
                 .distinct()                                 // - Оставить только уникальные книги
-                .filter(b -> b.getYear() > 2000)            // - Отфильтровать книги, оставив только выпущеные после 2000 года
+                .filter(book -> book.getYear() > 2000)      // - Отфильтровать книги, оставив только выпущеные после 2000 года
                 .limit(3)                           // - Ограничить стрим на 3 элементах
-                .map(b -> b.getYear())                      // - Получить из книг годы выпуска
-                // .forEach(System.out::println);
+                .map(Book::getYear)                         // - Получить из книг годы выпуска
 
                 .findFirst()                                // - При помощи методов короткого замыкания вернуть Optional от года
 
                 // - При помощи методов получения значения из Optional вывести в консоль год
                 // выпуска найденной книги,
-                .ifPresentOrElse(y -> System.out.println(String.format("Book year is %d", y)),
+                .ifPresentOrElse(year  -> System.out.println(String.format("Book year is %d", year)),
                         // либо запись о том, что такая книга отсутствует
                         () -> System.out.println("Book not found;"));
 
