e61e44d05f826db253b86a4e002bbca036de69d6
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-04-21 16:07:14 +0300

    Prepare to realisation Stratagies

--- Module3/anime-recommender/src/main/java/com/example/anime/strategy/RecommendationStrategy.java
+++ Module3/anime-recommender/src/main/java/com/example/anime/strategy/RecommendationStrategy.java
@@ -7,3 +7,33 @@ import java.util.List;
 public interface RecommendationStrategy {
     List<Anime> recommend(List<Anime> animeList);
 }
+
+public class RatingStrategy implements RecommendationStrategy {
+
+    @Override
+    public List<Anime> recommend(List<Anime> animeList) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recommend'");
+    }
+
+}
+
+public class GenreStrategy implements RecommendationStrategy {
+
+    @Override
+    public List<Anime> recommend(List<Anime> animeList) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recommend'");
+    }
+
+}
+
+public class PopularityStrategy implements RecommendationStrategy {
+
+    @Override
+    public List<Anime> recommend(List<Anime> animeList) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recommend'");
+    }
+
+}
