5534fae11b4a365b7c689a2f417df64b9ef946d0
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-04-22 12:17:51 +0300

    Update ExternalAnimeAPI Data

--- Module3/anime-recommender/src/main/java/com/example/anime/adapter/ExternalAnimeAPI.java
+++ Module3/anime-recommender/src/main/java/com/example/anime/adapter/ExternalAnimeAPI.java
@@ -6,8 +6,8 @@ public class ExternalAnimeAPI {
 
     public List<String> fetchAnimeData() {
         return List.of(
-                "Demon Slayer|Action|8.7|26|2019|Ufotable",
-                "One Piece|Adventure|8.9|1000|1999|Toei",
-                "Chainsaw Man|Action|8.5|12|2022|MAPPA");
+                "Demon Slayer|Action|8.41|26|2019|Ufotable",
+                "One Piece|Adventure|8.73|1158|1999|Toei",
+                "Chainsaw Man|Action|8.43|12|2022|MAPPA");
     }
 }
