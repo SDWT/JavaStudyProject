19d2baae6b4bee9dcddeb46ba7957c1389a2e80d
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-04-22 16:38:47 +0300

    Add decorators Test

--- Module3/anime-recommender/src/main/java/com/example/anime/Main.java
+++ Module3/anime-recommender/src/main/java/com/example/anime/Main.java
@@ -3,6 +3,11 @@ package com.example.anime;
 import com.example.anime.adapter.AnimeAPIAdapter;
 import com.example.anime.adapter.ExternalAnimeAPI;
 import com.example.anime.chain.*;
+import com.example.anime.decorator.AnimeComponent;
+import com.example.anime.decorator.AnimeComponentImpl;
+import com.example.anime.decorator.CommentDecorator;
+import com.example.anime.decorator.Empty2Decorator;
+import com.example.anime.decorator.FavoriteDecorator;
 import com.example.anime.model.Anime;
 import com.example.anime.service.AnimeService;
 import com.example.anime.service.impl.AnimeServiceProxy;
@@ -14,17 +19,29 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        // AnimeService service = new AnimeServiceProxy(new RealAnimeService());
+        AnimeService service = new AnimeServiceProxy(new RealAnimeService());
 
-        AnimeService service = new AnimeServiceProxy(
-                new AnimeAPIAdapter(new ExternalAnimeAPI()));
+        // AnimeService service = new AnimeServiceProxy(
+        // new AnimeAPIAdapter(new ExternalAnimeAPI()));
 
         List<Anime> list = service.getAllAnime();
         System.out.println("----");
 
         list = service.getAllAnime();
 
-        list.forEach(System.out::println);
+        list.stream().map(anime -> {
+            AnimeComponent component = new AnimeComponentImpl(anime);
+
+            if (anime.getTitle().contains("Friren"))
+                component = new FavoriteDecorator(component);
+            else
+                component = new Empty2Decorator(component);
+
+            if (anime.getRating() >= 8.7)
+                component = new CommentDecorator(component, "Must watch!");
+            return component.getDescription();
+        }).forEach(System.out::println);
+
         // List<RecommendationStrategy> strategies = List.of(
         // new RatingStrategy(),
         // new GenreStrategy("action"),
