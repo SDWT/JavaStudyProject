44c92154fff79c5307f4e83480923673a1181096
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-04-22 17:14:08 +0300

    Test RecommendationServiceImpl

--- Module3/anime-recommender/src/main/java/com/example/anime/Main.java
+++ Module3/anime-recommender/src/main/java/com/example/anime/Main.java
@@ -1,46 +1,32 @@
 package com.example.anime;
 
-import com.example.anime.adapter.AnimeAPIAdapter;
-import com.example.anime.adapter.ExternalAnimeAPI;
 import com.example.anime.chain.*;
-import com.example.anime.decorator.AnimeComponent;
-import com.example.anime.decorator.AnimeComponentImpl;
-import com.example.anime.decorator.CommentDecorator;
-import com.example.anime.decorator.Empty2Decorator;
-import com.example.anime.decorator.FavoriteDecorator;
-import com.example.anime.model.Anime;
 import com.example.anime.service.AnimeService;
+import com.example.anime.service.RecommendationService;
 import com.example.anime.service.impl.AnimeServiceProxy;
 import com.example.anime.service.impl.RealAnimeService;
-import com.example.anime.strategy.*;
-
-import java.util.List;
+import com.example.anime.service.impl.RecommendationServiceImpl;
+import com.example.anime.strategy.RatingStrategy;
+import com.example.anime.strategy.RecommendationStrategy;
 
 public class Main {
     public static void main(String[] args) {
 
         AnimeService service = new AnimeServiceProxy(new RealAnimeService());
-
         // AnimeService service = new AnimeServiceProxy(
         // new AnimeAPIAdapter(new ExternalAnimeAPI()));
 
-        List<Anime> list = service.getAllAnime();
-        System.out.println("----");
+        RecommendationService recommendationService = new RecommendationServiceImpl(service);
 
-        list = service.getAllAnime();
+        RecommendationStrategy strategy = new RatingStrategy();
 
-        list.stream().map(anime -> {
-            AnimeComponent component = new AnimeComponentImpl(anime);
+        FilterHandler chain = new GenreFilter("Action");
+        chain.setNext(new MinYearFilter(2009))
+                .setNext(new MinRatingFilter(8.52));
 
-            if (anime.getTitle().contains("Friren"))
-                component = new FavoriteDecorator(component);
-            else
-                component = new Empty2Decorator(component);
-
-            if (anime.getRating() >= 8.7)
-                component = new CommentDecorator(component, "Must watch!");
-            return component.getDescription();
-        }).forEach(System.out::println);
+        recommendationService.recommend(strategy,
+                chain, true, "Must watch!")
+                .forEach(a -> System.out.println(a.getDescription()));
 
         // List<RecommendationStrategy> strategies = List.of(
         // new RatingStrategy(),
@@ -55,12 +41,6 @@ public class Main {
         // result.forEach(System.out::println);
         // System.out.println();
         // }
-
-        // FilterHandler chain = new GenreFilter("Action");
-        // chain.setNext(new MinYearFilter(2009))
-        // .setNext(new MinRatingFilter(8.57));
-
-        // List<Anime> result = chain.handle(list);
 
         // result.forEach(System.out::println);
 
